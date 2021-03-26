b11d4ea7a416b118e467f6268b49fb79acb6b9f2
aqid <aa.qidd@gmail.com>
2021-03-26 23:28:13 +0700

    [update] rabbitmq sample

--- account/src/index.js
+++ account/src/index.js
@@ -1,29 +1,56 @@
-import express from 'express'
-import pkg from 'sqlite3'
-const { Database } = pkg
+import express from "express";
+import pkg from "sqlite3";
+import { connect } from "amqplib/callback_api.js";
+
+const { Database } = pkg;
 
 const app = express();
-const db = new Database('account.db')
+const db = new Database("account.db");
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
 app.listen(9000, () => {
-    db.run(`CREATE TABLE IF NOT EXISTS account(
+  db.run(`CREATE TABLE IF NOT EXISTS account(
         id              INTEGER     PRIMARY KEY     AUTOINCREMENT,
         name            TEXT,
         email           TEXT
-    )`)
+    )`);
 
-    console.log('Account Service is running in port 9000')
-})
+  console.log("Account Service is running in port 9000");
+});
 
-app.get('/accounts', (req, res) => {
-    db.all(`SELECT * FROM account`, (err, data) => {
-        // TODO use axios to get data from outlet
-        res.send(data)
-    })
-})
+app.get("/accounts", (req, res) => {
+  db.all(`SELECT * FROM account`, (err, data) => {
+    // TODO use axios to get data from outlet
+    res.send(data);
+  });
+});
+
+app.get("/send-event", (req, res) => {
+  const amqp = connect("amqp://rabbitmq", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "hello";
+      var msg = "Hello world NOW";
+
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+      
+      res.send('done sending event');
+    });
+  });
+
+});
 
 /**
  * to call this API via terminal, you can use CURL
@@ -34,13 +61,16 @@ app.get('/accounts', (req, res) => {
         "email": "john.doe@gmail.com"
     }'
  */
-app.post('/account', (req, res) => {
-    const body = req.body
-    db.run(`INSERT INTO account('name', 'email') VALUES ('${body.name}', '${body.email}')`, (err, data) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send('success!');
-        }
-    })
-})
+app.post("/account", (req, res) => {
+  const body = req.body;
+  db.run(
+    `INSERT INTO account('name', 'email') VALUES ('${body.name}', '${body.email}')`,
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("success!!");
+      }
+    }
+  );
+});
--- outlet/src/index.js
+++ outlet/src/index.js
@@ -1,29 +1,61 @@
-import express from 'express'
-import pkg from 'sqlite3'
-const { Database } = pkg
+import express from "express";
+import pkg from "sqlite3";
+import { connect } from "amqplib/callback_api.js";
+
+const { Database } = pkg;
 
 const app = express();
-const db = new Database('outlet.db')
+const db = new Database("outlet.db");
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
 app.listen(9001, () => {
-    db.run(`CREATE TABLE IF NOT EXISTS outlet(
+  db.run(`CREATE TABLE IF NOT EXISTS outlet(
         id              INTEGER     PRIMARY KEY     AUTOINCREMENT,
         name            TEXT,
         address           TEXT,
         account_id      INTEGER
-    )`)
+    )`);
 
-    console.log('Outlet Service is running in port 9001')
-})
+  // SUBSCRIBE TO RABBITMQ EVENT
+  connect("amqp://rabbitmq", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "hello";
 
-app.get('/outlets', (req, res) => {
-    db.all(`SELECT * FROM outlet`, (err, data) => {
-        res.send(data)
-    })
-})
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        queue
+      );
+      channel.consume(
+        queue,
+        function (msg) {
+          console.log(" [x] Received %s", msg.content.toString());
+        },
+        {
+          noAck: true,
+        }
+      );
+    });
+  });
+
+  console.log("Outlet Service is running in port 9001");
+});
+
+app.get("/outlets", (req, res) => {
+  db.all(`SELECT * FROM outlet`, (err, data) => {
+    res.send(data);
+  });
+});
 
 /**
  * to call this API via terminal, you can use CURL
@@ -35,13 +67,16 @@ app.get('/outlets', (req, res) => {
         "account_id": 1
     }'
  */
-app.post('/outlet', (req, res) => {
-    const body = req.body
-    db.run(`INSERT INTO outlet('name', 'address', 'account_id') VALUES ('${body.name}', '${body.address}', ${body.account_id})`, (err, data) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send('success!');
-        }
-    })
-})
+app.post("/outlet", (req, res) => {
+  const body = req.body;
+  db.run(
+    `INSERT INTO outlet('name', 'address', 'account_id') VALUES ('${body.name}', '${body.address}', ${body.account_id})`,
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("success!");
+      }
+    }
+  );
+});
